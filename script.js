03715a313e478ea213f35d7c3c70e9a4bb2db753
Chris Abiva <chrisabiva@hotmail.com>
2021-04-11 22:44:15 -0700

    Edited special character array. Avoiding [object] error

--- script.js
+++ script.js
@@ -14,7 +14,7 @@ function writePassword() {
 generateBtn.addEventListener("click", writePassword);
 
 // Array 
-var specialChar = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "_", "+", "-", "="];
+var specialChar = ["!", "@", "#", "$", "%", "^", "\&", "*", "(", ")", "_", "+", "-", "=", "\"", "'", "\\", "", "", "", ""];
 var numeric = ["0", "1", "3", "2", "4", "5", "6", "7", "8", "9"];
 var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 // Lower Case to Upper Case Array
